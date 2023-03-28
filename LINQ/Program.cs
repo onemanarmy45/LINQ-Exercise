3d6906b7333a68696a1d6e7f2de66e5dad959c7b
rob.jordan054@gmail.com <rob.jordan054@gmail.com>
2023-03-27 21:49:31 -0400

    video games

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿ namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Prince of Persia: Sands of Time", "Splinter Cell" };
+
+            videoGames.Add("Tecmo Bowl");
+            videoGames.Add("Super Smash Bros");
+
+            var orderOfGames = videoGames.OrderBy(name => name.Length);
+
+            foreach (var game in orderOfGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
